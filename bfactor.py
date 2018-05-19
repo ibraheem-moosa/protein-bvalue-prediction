248628746320e20cc94c96656987981c98ba0d35
ibraheemmoosa <ibraheemmoosa1347@gmail.com>
2018-05-19 23:31:04 +0600

    Removed some errors in bfactor.py

--- bfactor.py
+++ bfactor.py
@@ -5,7 +5,7 @@ from sklearn.model_selection import cross_val_score
 
 
 directory = "/home/ahsan/Desktop/MS Thesis/bfactor/output/" #appropriate location of output folder
-
+directory = './data/'
 
 amino_acids=dict(CYS = 0,
 TRP = 1,
@@ -35,105 +35,105 @@ other = 20
 
 def prepareDataSet():
 
-	SAMPLE_INTERVAL = 20
-	MAX_FILE_COUNT = 50
+    SAMPLE_INTERVAL = 20
+    MAX_FILE_COUNT = 50
 
-	FEATURE_DIMENSION = 63
+    FEATURE_DIMENSION = 63
 
-	X = np.empty((0,FEATURE_DIMENSION), float)
-	y = np.empty((0), float)
+    X = np.empty((0,FEATURE_DIMENSION), float)
+    y = np.empty((0), float)
 
-	fileCount = 0
+    fileCount = 0
 
-	for fileName in os.listdir(directory):
-		fileCount += 1;
+    for fileName in os.listdir(directory):
+        fileCount += 1;
 
-		if(fileCount==MAX_FILE_COUNT):
-			break
-		# print(fileName)
-		input = np.genfromtxt(directory+fileName, dtype= None, encoding=None)
+        if(fileCount==MAX_FILE_COUNT):
+            break
+        # print(fileName)
+        input = np.genfromtxt(directory+fileName, dtype= None, encoding=None)
 
-		totalAcids, totalFreq = calculateFrequency(input)
+        totalAcids, totalFreq = calculateFrequency(input)
 
-		# print(totalFreq, totalAcids)
+        # print(totalFreq, totalAcids)
 
-		freq={acid:0 for acid in amino_acids.keys()}
-		sampleCount = 0;
+        freq=dict((acid, 0) for acid in amino_acids.keys())
+        sampleCount = 0;
 
-		for(acid, bValue) in input:
-			if(acid not in amino_acids):
-				acid = "other"
-			if(acid not in freq):
-				freq[acid] = 0
-			freq[acid] += 1
+        for(acid, bValue) in input:
+            if(acid not in amino_acids):
+                acid = "other"
+            if(acid not in freq):
+                freq[acid] = 0
+            freq[acid] += 1
 
-			sampleCount += 1
-			if(sampleCount % SAMPLE_INTERVAL == 0):
-				sampleCount = 0
-				featureVector = extractFeature(freq, totalFreq, totalAcids, acid)
+            sampleCount += 1
+            if(sampleCount % SAMPLE_INTERVAL == 0):
+                sampleCount = 0
+                featureVector = extractFeature(freq, totalFreq, totalAcids, acid)
 
-				# print(featureVector)
+                # print(featureVector)
 
 
-				X = np.append(X, featureVector, axis=0)
+                X = np.append(X, featureVector, axis=0)
 
-				y = np.append(y, bValue)
-		# print(X)
-		# print(y)
+                y = np.append(y, bValue)
+        # print(X)
+        # print(y)
 
-	return X, y
+    return X, y
 
 
 
 def calculateFrequency(input):
-		freq={acid:0 for acid in amino_acids.keys()}
-		totalAcids = 0;
-		for(acid, bValue) in input:
-			if(acid not in amino_acids):
-				acid = "other"
-			if(acid not in freq):
-				freq[acid] = 0
-			freq[acid] += 1
+        freq=dict((acid, 0) for acid in amino_acids.keys())
+        totalAcids = 0;
+        for(acid, bValue) in input:
+            if(acid not in amino_acids):
+                acid = "other"
+            if(acid not in freq):
+                freq[acid] = 0
+            freq[acid] += 1
 
-			totalAcids += 1
+            totalAcids += 1
 
-		return totalAcids, freq;
+        return totalAcids, freq;
 
 
 
 def extractFeature(freq, totalFreq, totalAcids, acid_name):
-	featureVector = [(freq[acid]*1.0/totalAcids) for acid in amino_acids.keys()]
+    featureVector = [(freq[acid]*1.0/totalAcids) for acid in amino_acids.keys()]
 
 
- 	featureVector.extend([totalFreq[acid]*1.0/totalAcids for acid in amino_acids.keys()])
+    featureVector.extend([totalFreq[acid]*1.0/totalAcids for acid in amino_acids.keys()])
 
 
- 	if acid in amino_acids:
- 		x = np.array([acid for acid in amino_acids.keys()]) == acid_name
- 	else:
- 		x = np.array([acid for acid in amino_acids.keys()]) == "other"
+    if acid_name in amino_acids:
+        x = np.array([acid for acid in amino_acids.keys()]) == acid_name
+    else:
+        x = np.array([acid for acid in amino_acids.keys()]) == "other"
 
- 	# print(acid_name, x)
+    # print(acid_name, x)
 
- 	featureVector.extend(x)
+    featureVector.extend(x)
 
-	return np.array([featureVector])
+    return np.array([featureVector])
 
 
 
 
 def main():
-	X, y = prepareDataSet()
+    X, y = prepareDataSet()
 
-	print(len(y))
+    print(len(y))
 
 
-	clf = SVR(C=1.0, epsilon=0.2)
+    clf = SVR(C=1.0, epsilon=0.2)
 
-	# clf.fit(X[:20], y[:20])
+    # clf.fit(X[:20], y[:20])
 
-	scores = cross_val_score(clf, X, y, cv=10)
-	print(scores)
+    scores = cross_val_score(clf, X, y, cv=10)
+    print(scores)
 
 if __name__ == "__main__":
     main()
