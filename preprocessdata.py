3b0338c74412294a01258bcfa25c9b010d745b20
ibraheemmoosa <ibraheemmoosa1347@gmail.com>
2018-05-28 09:44:06 +0600

    Local frequency window size can be given seperately

--- preprocessdata.py
+++ preprocessdata.py
@@ -1,6 +1,6 @@
 import sys
 if len(sys.argv) < 6:
-    print('Usage: python3 preprocessdata.py input_directory num_of_proteins window_size X.txt y.txt')
+    print('Usage: python3 preprocessdata.py input_directory num_of_proteins window_size local_freq_window_size X.txt y.txt')
     print('Example: python3 preprocessdata.py data 350 15 X.txt y.txt')
     exit()
 
@@ -16,6 +16,7 @@ from collections import Counter
 num_of_proteins = int(sys.argv[2])
 files = random.sample(os.listdir(sys.argv[1]), num_of_proteins)
 window_size = int(sys.argv[3])
+local_freq_ws = int(sys.argv[4])
 
 def aa_to_index(aa):
     """
@@ -41,6 +42,25 @@ def seq_to_windows(seq, window_size):
     windows.extend([seq[i - window_size:] + [20] * (i + window_size - len(seq) + 1) for i in range(len(seq) - window_size, len(seq))])
     return windows
 
+def seq_to_freq(seq):
+    freq = [0.0] * 21
+    for aa in seq:
+        freq[aa] += 1
+    freq = [freq[i] / len(seq) for i in range(len(seq))]
+    return freq
+
+def seq_to_local_freq(seq, window_size):
+    local_freqs = []
+    for i in range(len(seq)):
+        if i < window_size:
+            local_seq = [20] * (window_size - i) + seq[:i + window_size + 1]
+        elif i + window_size > len(seq) - 1:
+            local_seq = seq[i - window_size:] + [20] * (i + window_size - len(seq) + 1)
+        else:
+            local_seq = seq[i - window_size: i + window_size + 1]
+        local_freqs.append((seq_to_freq(local_seq)))
+    return local_freqs
+
 
 X = []
 y = []
@@ -54,23 +74,20 @@ for f in files:
         b = float(b)
         seq.append(aa_to_index(a))
         bfactors.append(b)
-    aa_counter = Counter(seq)
-    aa_freq = [aa_counter[aa] / len(seq) for aa in range(21)] 
     windows = seq_to_windows(seq, window_size)
     input_data = windows
     # additional features
-     
+    local_freqs = seq_to_local_freq(seq, local_freq_ws)
+    global_freq = seq_to_freq(seq)
     protein_len = len(input_data)
     for i, data in enumerate(input_data):
-        local_aa_counter = Counter(data)
-        local_aa_freq = [local_aa_counter[aa] / len(data) for aa in range(21)] 
         pos = i / protein_len
         # relative position
         data.append(pos)
         # global amino acid frequency
         data.extend(aa_freq)
-        # amino acid frequencey in the window
-        data.extend(local_aa_freq)
+        # local amino acid frequencey
+        data.extend(local_aa_freqs[i])
     X.extend(input_data)
     bfactors = np.array(bfactors)
     bfactors = (bfactors - np.mean(bfactors)) / np.std(bfactors)
@@ -83,7 +100,7 @@ onehot = lambda x : ''.join(['1' if i == x else '0' for i in range(21)])
 five_len_bin = lambda br: '0' * (5 - len(br)) + br
 
 # write X
-with open(sys.argv[4], 'w') as f:
+with open(sys.argv[5], 'w') as f:
     for i, x in enumerate(X):
         to_write = ''
         
@@ -97,7 +114,7 @@ with open(sys.argv[4], 'w') as f:
         f.write(to_write + '\n')
 
 # write y
-with open(sys.argv[5], 'w') as f:
+with open(sys.argv[6], 'w') as f:
     f.writelines(map(lambda _y: str(_y) + '\n', y))
 
 
