07eaba2be936e70339fbd5b05626413f8db36229
ibraheemmoosa <ibraheemmoosa1347@gmail.com>
2018-06-27 10:55:52 +0600

    Implmented seperate fixed width feedforward network

--- src/torch_rnn.py
+++ src/torch_rnn.py
@@ -8,56 +8,101 @@ import torch.utils.data
 import scipy.sparse as scsp
 from bisect import bisect
 
+class FixedWidthFeedForwardNeuralNetwork(nn.Module):
+    def __init__(self, width, num_outputs, num_layers, activation):
+        super(FixedWidthFeedForwardNeuralNetwork, self).__init__()
+        self.linear_layers = [nn.Linear(width, width) for i in range(num_layers-1)]
+        self.linear_layers.append(nn.Linear(width, num_outputs))
+        self.activation = activation
+        
+    def forward(self, x):
+        out = self.activation(self.linear_layers[0](x))
+        for i in range(1, len(self.linear_layers) - 1):
+            out = self.activation(self.linear_layers[i](out))
+        out = self.linear_layers[-1](out)
+        return out
+
 class RecurrentNeuralNetwork(nn.Module):
-    def __init__(self, input_size, hidden_size=8, hidden_scale=1.0, ff_scale=0.001):
+    def __init__(self, input_size, hidden_size=8, hidden_scale=1.0, num_hidden_layers=1, ff_scale=0.001, output_layer_depth=1):
         super(RecurrentNeuralNetwork, self).__init__()
-        self.layer1 = nn.RNN(input_size=input_size, hidden_size=hidden_size, nonlinearity='relu', batch_first=True, bidirectional=False)
-        self.layer2 = nn.Linear(hidden_size, 1)
+        self.hidden_size = hidden_size
+        self.num_hidden_layers = num_hidden_layers
+        self.rnn_layer = nn.RNN(input_size=input_size, hidden_size=hidden_size, 
+                nonlinearity='relu', num_layers=num_hidden_layers, batch_first=True, bidirectional=False)
+        self.output_layer = FixedWidthFeedForwardNeuralNetwork(hidden_size, 1, output_layer_depth, leaky_relu)
         self._init_weights_()
 
     def forward(self, x):
-        out, h = self.layer1(x, torch.zeros(1, 1, self.layer1.hidden_size))
-        out = self.layer2(out)
+        out, h = self.rnn_layer(x, torch.zeros(self.num_hidden_layers, x.shape[0], self.hidden_size))
+        out = self.output_layer(out)
         return out
     
     def _init_weights_(self):
         ff_init_method = nn.init.normal_
-        init_method = nn.init.constant_
+        hidden_weight_init_method = nn.init.eye_
         bias_init_method = nn.init.constant_
-        torch.eye(hidden_size, out=self.layer1.weight_hh_l0, requires_grad=True)
-        with  torch.no_grad():
-            self.layer1.weight_hh_l0.mul_(hidden_scale)
-        bias_init_method(self.layer1.bias_hh_l0, 0)
-        ff_init_method(self.layer1.weight_ih_l0, std=ff_scale)
-        bias_init_method(self.layer1.bias_ih_l0, 0)
-        ff_init_method(self.layer2.weight, std=ff_scale)
-        bias_init_method(self.layer2.bias, 0)
+        for name, param in self.rnn_layer.named_parameters():
+            if 'weight_hh' in name:
+                hidden_weight_init_method(param)
+                with torch.no_grad():
+                    param.mul_(hidden_scale)
+                param.requires_grad_()
+            elif 'weight_ih' in name:
+                ff_init_method(param, std=ff_scale)
+            else:
+                bias_init_method(param, 0)
+        
+        for name, param in self.output_layer.named_parameters():
+            if 'weight' in name:
+                ff_init_method(param, std=ff_scale)
+            else:
+                bias_init_method(param, 0)
 
     def predict(self, x):
         with torch.no_grad():
-            out, h = self.layer1(x, torch.zeros(1, 1, self.layer1.hidden_size))
-            out = self.layer2(out)
+            out = self.forward(x)
             return out
+
 
 class ProteinDataset(torch.utils.data.Dataset):
 
     def __init__(self, X_files, y_files):
-        self._protein_lengths = []
         self._Xes = []
         self._yes = []
         for xf,yf in zip(X_files, y_files):
             X = torch.from_numpy(scsp.load_npz(xf).toarray()).reshape((-1, 21))
             y = torch.from_numpy(np.load(yf)['y']).reshape((-1, 1))
-            assert(X.shape[0] == len(y))
-            self._protein_lengths.append(len(y))
+            assert(X.shape[0] == y.shape[0])
             self._Xes.append(X)
             self._yes.append(y)
-
+        self._Xes = sorted(self._Xes, key=lambda x: x.shape[0])
+        self._yes = sorted(self._yes, key=lambda y: y.shape[0])
+        to_be_collated_Xes = [self._Xes[0]]
+        to_be_collated_yes = [self._yes[0]]
+        collated_Xes = []
+        collated_yes = []
+        for X, y in zip(self._Xes, self._yes):
+            assert(X.shape[0] == y.shape[0])
+            if X.shape[0] == to_be_collated_Xes[-1].shape[0]:
+                assert(y.shape[0] == to_be_collated_yes[-1].shape[0])
+                to_be_collated_Xes.append(X)
+                to_be_collated_yes.append(y)
+            else:
+                collated_Xes.append(torch.stack(to_be_collated_Xes))
+                collated_yes.append(torch.stack(to_be_collated_yes))
+                to_be_collated_Xes = [X]
+                to_be_collated_yes = [y]
+        self._Xes = collated_Xes
+        self._yes = collated_yes
+        
     def __getitem__(self, idx):
         return self._Xes[idx], self._yes[idx]
 
     def __len__(self):
-        return len(self._protein_lengths)
+        return len(self._Xes)
+
+def summarize_tensor(tensor):
+    return torch.max(tensor).item(), torch.min(tensor).item(), torch.mean(tensor).item(), torch.std(tensor).item()
 
 if __name__ == '__main__':
 
@@ -102,7 +147,6 @@ if __name__ == '__main__':
     y_validation_files = y_files[-len(indices) // 5:]
     X_files = X_files[:-len(indices) // 5]
     y_files = y_files[:-len(indices) // 5]
-    indices = list(range(len(X_files)))
     dataset = ProteinDataset(X_files, y_files)
     print('Dataset init done ', len(dataset))
     validation_dataset = ProteinDataset(X_validation_files, y_validation_files)
@@ -117,16 +161,18 @@ if __name__ == '__main__':
         warm_start_last_epoch = -1
 
     batch_size = 1
-    dataloader = torch.utils.data.DataLoader(dataset, batch_size=batch_size, num_workers=4, shuffle=True)
-    validation_dataloader = torch.utils.data.DataLoader(validation_dataset, batch_size=batch_size, num_workers=4)
+    #dataloader = torch.utils.data.DataLoader(dataset, batch_size=batch_size, num_workers=4, shuffle=True)
+    #validation_dataloader = torch.utils.data.DataLoader(validation_dataset, batch_size=batch_size, num_workers=4)
 
-    init_lr = 0.001
+    init_lr = 0.005
     momentum = 0.9
-    weight_decay = 0.0
+    weight_decay = 0.001
     hidden_size = 8
     hidden_scale = 1.0
+    num_hidden_layers = 4
+    output_layer_depth = 4
     ff_scale = 0.1
-    grad_clip = 1000.0
+    grad_clip = 1.0
     nesterov = True
 
     print('Initial LR: {}'.format(init_lr))
@@ -136,80 +182,84 @@ if __name__ == '__main__':
     print('Hidden Scale: {}'.format(hidden_scale))
     print('FF Scale: {}'.format(ff_scale))
     print('Hidden Layer Size: {}'.format(hidden_size))
+    print('Output Layer Depth: {}'.format(output_layer_depth))
+    print('Number of Hidden Layers: {}'.format(num_hidden_layers))
+    print('Three hidden layer at output')
     #print('Nesterov: {}'.format(nesterov))
-    print('Adam')
+    print('Adam, Amsgrad')
+    #print('Adadelta')
 
-    net = RecurrentNeuralNetwork(21, hidden_size, hidden_scale, ff_scale)
+    net = RecurrentNeuralNetwork(21, hidden_size, hidden_scale, num_hidden_layers, ff_scale, output_layer_depth)
     if warm_start_model_params != None:
         net.load_state_dict(warm_start_model_params)
 
     from math import sqrt
     import torch.optim as optim
     criterion = nn.MSELoss()
-    optimizer = optim.SGD([{'params' : net.parameters(), 'initial_lr' : init_lr}],
-                            lr=init_lr, momentum=momentum, weight_decay=weight_decay, nesterov=nesterov)
+    #optimizer = optim.SGD([{'params' : net.parameters(), 'initial_lr' : init_lr}],
+    #                        lr=init_lr, momentum=momentum, weight_decay=weight_decay, nesterov=nesterov)
+    #scheduler = optim.lr_scheduler.ExponentialLR(optimizer, 0.99)
     optimizer = optim.Adam([{'params' : net.parameters(), 'initial_lr' : init_lr}], 
-                            lr=init_lr, weight_decay=weight_decay)
+                            lr=init_lr, weight_decay=weight_decay, amsgrad=True)
+    #optimizer = optim.Adadelta([{'params' : net.parameters(), 'initial_lr' : init_lr}], 
+    #                        lr=init_lr, weight_decay=weight_decay)
+
 
     from sklearn.metrics import r2_score
     from scipy.stats import pearsonr
+
+    indices = list(range(len(dataset)))
     for epoch in range(warm_start_last_epoch + 1, warm_start_last_epoch + 1 + 500):
+        #scheduler.step()
         running_loss = 0.0
-        layer1_hidden_grad = torch.zeros_like(net.layer1.weight_hh_l0)
-        layer1_hidden_bias_grad = torch.zeros_like(net.layer1.bias_hh_l0)
-        layer1_input_grad = torch.zeros_like(net.layer1.weight_ih_l0)
-        layer1_input_bias_grad = torch.zeros_like(net.layer1.bias_ih_l0)
-        layer2_bias_grad = torch.zeros_like(net.layer2.bias)
-        layer2_grad = torch.zeros_like(net.layer2.weight)
-        for i, data in enumerate(dataloader):
-            x, y = data
+        random.shuffle(indices)
+        for i in indices:
+            x, y = dataset[i]
             optimizer.zero_grad()
             y_pred = net(x)
             loss = criterion(y_pred, y)
             loss.backward()
             nn.utils.clip_grad_value_(net.parameters(), grad_clip)
-            layer1_hidden_bias_grad += net.layer1.bias_hh_l0.grad
-            layer1_hidden_grad += net.layer1.weight_hh_l0.grad
-            layer1_input_bias_grad += net.layer1.bias_ih_l0.grad
-            layer1_input_grad += net.layer1.weight_ih_l0.grad
-            layer2_bias_grad += net.layer2.bias.grad
-            layer2_grad += net.layer2.weight.grad
             optimizer.step()
             running_loss += loss.item()
-
-        layer1_hidden_grad /= len(dataset)
-        layer1_hidden_bias_grad /= len(dataset)
-        layer1_input_grad /= len(dataset)
-        layer1_input_bias_grad /= len(dataset)
-        layer2_grad /= len(dataset)
-        layer2_bias_grad /= len(dataset)
 
         print(time.strftime('%Y-%m-%d %H:%M'))
         print('Epoch: {} done. Loss: {}'.format(
                                 epoch, running_loss / len(dataset)))
-        print('\tInput Weight Grad: {}'.format(layer1_input_grad))
-        print('\tInput Weight: {}'.format(net.layer1.weight_ih_l0))
-        print('\tInput Bias Grad: {}'.format(layer1_input_bias_grad))
-        print('\tInput Bias: {}'.format(net.layer1.bias_ih_l0))
+        print('##############################################################')
+        for name, param in net.named_parameters():
+            print('---------------------------------------------------------------')
+            print(name)
+            print(summarize_tensor(param))
+        print('##############################################################')
+
+        '''
+        print('\tInput Weight Grad: {}'.format(summarize_tensor(layer1_input_grad)))
+        print('\tInput Weight: {}'.format(summarize_tensor(net.layer1.weight_ih_l0)))
+        print('\tInput Bias Grad: {}'.format(summarize_tensor(layer1_input_bias_grad)))
+        print('\tInput Bias: {}'.format(summarize_tensor(net.layer1.bias_ih_l0)))
         
-        print('\tHidden Weight Grad: {}'.format(layer1_hidden_grad))
-        print('\tHidden Weight: {}'.format(net.layer1.weight_hh_l0))
-        print('\tHidden Bias Grad: {}'.format(layer1_hidden_bias_grad))
-        print('\tHidden Bias: {}'.format(net.layer1.bias_hh_l0))
+        print('\tHidden Weight Grad: {}'.format(summarize_tensor(layer1_hidden_grad)))
+        print('\tHidden Weight: {}'.format(summarize_tensor(net.layer1.weight_hh_l0)))
+        print('\tHidden Bias Grad: {}'.format(summarize_tensor(layer1_hidden_bias_grad)))
+        print('\tHidden Bias: {}'.format(summarize_tensor(net.layer1.bias_hh_l0)))
         
-        print('\tOutput Weight Grad: {}'.format(layer2_grad))
-        print('\tOutput Weight: {}'.format(net.layer2.weight))
-        print('\tOutput Bias Grad: {}'.format(layer2_bias_grad))
-        print('\tOutput Bias: {}'.format(net.layer2.bias))
+        print('\tOutput Weight Grad: {}'.format(summarize_tensor(layer2_grad)))
+        print('\tOutput Weight: {}'.format(summarize_tensor(net.layer2.weight)))
+        print('\tOutput Bias Grad: {}'.format(summarize_tensor(layer2_bias_grad)))
+        print('\tOutput Bias: {}'.format(summarize_tensor(net.layer2.bias)))
+        '''
 
         validation_pcc = []
-        for x, y in validation_dataloader:
+        for i in range(len(validation_dataset)):
+            x, y = validation_dataset[i]
             y_pred = net.predict(x)
             validation_pcc.append(pearsonr(y_pred.numpy().flatten(), y.numpy().flatten())[0])
         validation_pcc = np.array(validation_pcc)
         validation_pcc[np.isnan(validation_pcc)] = 0
         train_pcc = []
-        for x, y in dataloader:
+        for i in range(len(dataset)):
+            x, y = dataset[i]
             y_pred = net.predict(x)
             train_pcc.append(pearsonr(y_pred.numpy().flatten(), y.numpy().flatten())[0])
         train_pcc = np.array(train_pcc)
