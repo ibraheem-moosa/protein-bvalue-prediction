0c0913a9e9249d83210bf6569dc3561093a8d448
Ibraheem Moosa <ibraheemmoosa1347@gmail.com>
2018-08-21 21:32:11 +0600

    Per protein pcc average reported

--- src/regression.py
+++ src/regression.py
@@ -20,12 +20,12 @@ from sklearn.ensemble import GradientBoostingRegressor
 
 
 X_train = load(sys.argv[1])['X']
-X_test = load(sys.argv[3])['X'][0:4000]
+X_test = load(sys.argv[3])['X']
 print(X_train.shape)
 print(X_test.shape)
 
 y_train = load(sys.argv[2])['y']
-y_test = load(sys.argv[4])['y'][0:4000]
+y_test = load(sys.argv[4])['y']
 
 #y_train = (y_train - y_train.mean()) / y_train.std()
 #y_test = (y_test - y_test.mean()) / y_test.std()
@@ -40,36 +40,45 @@ else:
 SLIDING_WINDOW_SIZE = (X_test.shape[1]- 1 - 1 - 21 - 21)//3; # must match the equation in rfpreprocessor.py
 print("sliding window size:", SLIDING_WINDOW_SIZE)
 
-def test(clf, X_test):
+def test(clf, X_test, y_test):
     y_pred = []
+    y_preds = []
+    y_trues = []
     ys = []
+    y_true = []
     count = 0
-    for x in X_test:
+    for x, y in zip(X_test, y_test):
         if(x[-1] < -1000):
             count += 1
+            if len(ys) > 0:
+                y_preds.append(ys)
+                y_trues.append(y_true)
             ys = []
+            y_true = []
             print("testing sample {}".format(count))
         if(len(ys) >= SLIDING_WINDOW_SIZE):
     	    x[-SLIDING_WINDOW_SIZE:-1] = ys[-SLIDING_WINDOW_SIZE:-1]
         elif(len(ys) > 0):
     	    x[-len(ys)-1:-1] = ys
-        y = clf.predict([x])
-        y_pred.append(y[0])
-        ys.append(y[0])
+        y_true.append(y)
+        y = clf.predict([x])[0]
+        y_pred.append(y)
+        ys.append(y)
     
     print("tested {} samples".format(count))
     
-    return y_pred
+    return y_pred, y_preds, y_trues
 
 
-clf = RandomForestRegressor(n_estimators=400, n_jobs=4, verbose=0, max_depth=4, max_features='sqrt')
+clf = RandomForestRegressor(n_estimators=100, n_jobs=4, verbose=5, max_depth=4, max_features='sqrt', random_state=seed)
 print(clf)
 
 clf.fit(X_train, y_train)
 gc.collect()
 print("training done.........................")
-
-y_test_pred = test(clf, X_test) #clf.predict(X_test)
+clf.verbose = 0
+clf.n_jobs = 1
+y_test_pred, y_test_preds, y_test_trues = test(clf, X_test, y_test) #clf.predict(X_test)
 #y_train_pred = test(clf, X_train) #clf.predict(X_train)
 gc.collect()
 
@@ -84,7 +93,12 @@ from scipy.stats import pearsonr
 
 def p(y_pred,y_true):
     return pearsonr(y_pred,y_true)[0]
-
+def avg_p(y_preds, y_trues):
+    pcc = 0
+    for y_pred, y_true in zip(y_preds, y_trues):
+        pcc += p(y_pred, y_true)
+    pcc /= len(y_preds)
+    return pcc
 
 
 #print('Train Pearson Score: {}'.format(p(y_train, y_train_pred)))
@@ -93,7 +107,8 @@ def p(y_pred,y_true):
 #print('Train Data Mean: {} Standard Deviation: {}'.format(np.mean(y_train), np.std(y_train)))
 #print('Train Predicted Mean: {} Standard Deviation: {}'.format(np.mean(y_train_pred), np.std(y_train_pred)))
 
-print('Test Pearson Score: {}'.format(p(y_test, y_test_pred)))
+print('Test Pearson Score (avg): {}'.format(avg_p(y_test_trues, y_test_preds)))
+print('Test Pearson Score (con): {}'.format(p(y_test, y_test_pred)))
 print('Test Error: {}'.format(mean_squared_error(y_test, y_test_pred) / 2.0))
 #print('Test R2 Score:: {}'.format(clf.score(X_test, y_test)))
 print('Test Data Mean: {} Standard Deviation: {}'.format(np.mean(y_test), np.std(y_test)))
