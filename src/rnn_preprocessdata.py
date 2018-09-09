Pearson calculation working, loss calculation fixed

src/rnn_preprocessdata.py
@@ -42,7 +42,7 @@ if __name__ == '__main__':
 
     import os
 
-    protein_list = open(sys.argv[3]).read().split(',')
+    protein_list = open(sys.argv[3]).read().split('\n')
     print('Total proteins:', len(protein_list))
 
     currently_processing = 0
@@ -73,8 +73,8 @@ if __name__ == '__main__':
         X = protein_to_features(seq)
         assert(X.shape[0] == len(bfactors))
         if target_available:
-            bfactors = np.log(np.array(bfactors))
-            bfactors = (bfactors - np.mean(bfactors)) / np.std(bfactors)
+            #bfactors = np.log(np.array(bfactors))
+            #bfactors = (bfactors - np.mean(bfactors)) / np.std(bfactors)
             bfactors = np.array(bfactors, dtype=np.float32)
 
         out_fname_suffix = protein.upper() + '_rnn_'

src/torch_helper.py
@@ -39,13 +39,15 @@ def summarize_nn(net):
         print(summarize_tensor(param))
     print('##############################################################')
 
-def get_avg_pcc(net, dataset, indices):
-    pcc = []
-    for i in indices:
-        x, y = dataset[i]
-        y_pred = net.predict(x)
-        for j in range(x.shape[0]):
-            pcc.append(pearsonr(y_pred.numpy()[j].flatten(), y.numpy()[j].flatten())[0])
+def get_avg_pcc(y_true, y_pred, lengths):
+    pcc =[]
+    y_true = y_true.numpy()
+    y_pred = y_pred.detach().numpy()
+    for i in range(len(lengths)):
+        l = lengths[i]
+        yt = y_true[i].flatten()
+        yp = y_pred[i].flatten()
+        pcc.append(pearsonr(yt[:l], yp[:l])[0])
 
     pcc = np.array(pcc)
     pcc[np.isnan(pcc)] = 0

src/torch_lstm.py
@@ -29,6 +29,14 @@ if __name__ == '__main__':
         print('Usage: python3 regression.py data_dir protein_list checkpoint_dir [warm_start_model] [warm_start_epoch]')
         exit()
 
+    if len(sys.argv) == 6:
+        warm_start_model_params = torch.load(sys.argv[4])
+        warm_start_last_epoch = int(sys.argv[5])
+    else:
+        warm_start_model_params = None
+        warm_start_last_epoch = -1
+
+
     print(time.strftime('%Y-%m-%d %H:%M'))
 
     with open(sys.argv[2]) as protein_list_file:
@@ -38,7 +46,6 @@ if __name__ == '__main__':
     indices = list(range(len(protein_list)))
     random.seed(42)
     random.shuffle(indices)
-    indices = indices[:300]
     train_indices = indices[:int(0.8 * len(indices))]
     test_indices = indices[int(0.8 * len(indices)):]
 
@@ -50,35 +57,32 @@ if __name__ == '__main__':
         y_files.append(os.path.join(sys.argv[1], 'y_' + protein + '_rnn_.npz'))
 
     files = list(zip(X_files, y_files))
-    dataset = ProteinDataset([files[i] for i in train_indices], 16)
+    
+    batch_size = 32
+    
+    dataset = ProteinDataset([files[i] for i in train_indices], batch_size)
     print('Dataset init done ', len(dataset))
-    test_dataset = ProteinDataset([files[i] for i in test_indices], 16)
+    test_dataset = ProteinDataset([files[i] for i in test_indices], batch_size)
     print('Test Dataset init done ', len(test_dataset))
     print(time.strftime('%Y-%m-%d %H:%M'))
     
-    if len(sys.argv) == 6:
-        warm_start_model_params = torch.load(sys.argv[4])
-        warm_start_last_epoch = int(sys.argv[5])
-    else:
-        warm_start_model_params = None
-        warm_start_last_epoch = -1
-
-    batch_size = 1
-    init_lr = 1.0 / 128
+    init_lr = 2.0 ** -16
     momentum = 0.9
-    weight_decay = 1e-3
+    weight_decay = 10
     gamma = 0.9
-    hidden_size = 8
+    hidden_size = 64
     hidden_scale = 1.0
     num_hidden_layers = 1
-    output_layer_depth = 2
+    output_layer_depth = 8
     ff_scale = 0.6
     grad_clip = 10.0
     nesterov = True
 
     print('Initial LR: {}'.format(init_lr))
+    print('Batch Size: {}'.format(batch_size))
     #print('Momentum: {}'.format(momentum))
     print('Weight Decay: {}'.format(weight_decay))
+    print('Gamma: {}'.format(gamma))
     print('Gradient Clip: {}'.format(grad_clip))
     print('Hidden Scale: {}'.format(hidden_scale))
     print('FF Scale: {}'.format(ff_scale))
@@ -98,7 +102,7 @@ if __name__ == '__main__':
     if warm_start_model_params != None:
         net.load_state_dict(warm_start_model_params)
 
-    train_mses, test_mses = net.train(dataset, test_dataset, sys.argv[3])
+    train_mses, test_mses = net.train(dataset, test_dataset, sys.argv[3], patience=20)
     print(list(zip(train_mses, test_mses)))
     #train_nn(net, dataset, train_indices, validation_indices, sys.argv[3])
     #scores = cross_validation(net, dataset, indices, 10, 0.40)

src/torch_rnn.py
@@ -117,7 +117,7 @@ class RecurrentNeuralNetwork(nn.Module):
         self.init_layers()
 
     def train(self, dataset, validation_dataset, model_dir=None, patience=5, warm_start_last_epoch=-1):
-        criterion = nn.MSELoss()
+        criterion = nn.MSELoss(reduction='sum')
         optimizer = optim.Adam([{'params' : self.parameters(), 'initial_lr' : self.init_lr}], 
                         lr=self.init_lr, weight_decay=self.weight_decay, amsgrad=False)
         scheduler = optim.lr_scheduler.ExponentialLR(optimizer, self.gamma)
@@ -128,10 +128,11 @@ class RecurrentNeuralNetwork(nn.Module):
         best_epoch = 0
         best_mse = 1000.0
         validation_mses = []
+        validation_pccs = []
         train_mses = []
-        for epoch in range(warm_start_last_epoch + 1, warm_start_last_epoch + 1 + 20):
+        train_pccs = []
+        for epoch in range(warm_start_last_epoch + 1, warm_start_last_epoch + 1 + 1000):
             scheduler.step()
-            running_loss = 0.0
             for i in range(num_of_batches):
                 x, y, lengths = dataset[i]
                 optimizer.zero_grad()
@@ -140,26 +141,42 @@ class RecurrentNeuralNetwork(nn.Module):
                 loss.backward()
                 nn.utils.clip_grad_value_(self.parameters(), self.grad_clip)
                 optimizer.step()
-                running_loss += loss.item()
-            running_loss /= num_of_batches
-            train_mses.append(running_loss)
+
+            train_loss = 0.0
+            mean_pcc = 0.0
+            for i in range(num_of_batches):
+                x, y, lengths = dataset[i]
+                y_pred = self.predict(x, lengths)
+                loss = criterion(y_pred, y)
+                train_loss += loss.item()
+                mean_pcc += get_avg_pcc(y, y_pred, lengths)
+            train_loss /= dataset.total_length()
+            mean_pcc /= num_of_batches
+            train_mses.append(train_loss)
+            train_pccs.append(mean_pcc)
 
             validation_loss = 0.0
+            mean_pcc = 0.0
             for i in range(validation_num_of_batches):
                 x, y, lengths = validation_dataset[i]
                 y_pred = self.predict(x, lengths)
                 loss = criterion(y_pred, y)
                 validation_loss += loss.item()
-            validation_loss /= validation_num_of_batches
+                mean_pcc += get_avg_pcc(y, y_pred, lengths)
+            validation_loss /= validation_dataset.total_length()
+            mean_pcc /= validation_num_of_batches
             validation_mses.append(validation_loss)
+            validation_pccs.append(mean_pcc)
 
-            if validation_loss < best_mse:
-                best_mse = validation_loss
+            if train_loss < best_mse:
+                best_mse = train_loss
                 best_epoch = epoch
                 print(best_epoch)
             
-            print('Epoch: {0:02d} Loss: {1:.6f} Validation Loss: {2:.6f} Time: {3}'.format(
-                                    epoch, running_loss, validation_loss, time.strftime('%Y-%m-%d %H:%M:%S')))
+            print('Epoch: {0:02d} Time: {1} Loss: {2:.6f} Test Loss: {3:.6f} PCC: {4:0.6f} Test PCC: {5:0.6f}'.format(
+                                    epoch, time.strftime('%Y-%m-%d %H:%M:%S'), 
+                                    train_loss, validation_loss, 
+                                    train_pccs[-1], validation_pccs[-1]))
 
             if model_dir is not None:
                 torch.save(self.state_dict(), os.path.join(model_dir, 'net-{0:02d}'.format(epoch)))