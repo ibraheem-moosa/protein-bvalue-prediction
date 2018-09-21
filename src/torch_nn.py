a4a34ba252e0848b0dc2caf415057e94f435873e
Ibraheem Moosa <ibraheemmoosa1347@gmail.com>
2018-09-21 19:04:10 +0600

    Dataset implemented

--- src/torch_nn.py
+++ src/torch_nn.py
@@ -78,6 +78,84 @@ class FeedForward(nn.Module):
             dataset.reset()
 
 
+class Dataset:
+
+    def __init__(self, X, y, ws):
+        self.lengths = list(map(len, y))
+        self.length = sum(lengths)
+        self.ws = ws
+        self.y = []
+        index = [[], []]
+        value = []
+        num_column = 21 * (2 * ws + 1) + ws
+        current_row = 0
+        for i in range(len(X)):
+            self.y.extend(y[i])
+            aa_in_window = [20] * ws + X[i][:ws + 1]
+            for j in range(len(X[i])):
+                value.extend([1] * (2 * ws + 1))
+                value.extend([0] * ws)
+                index[0].extend([1] * (3 * ws + 1))
+                index[1].extend(list(map(lambda t: 21 * t[0] + t[1], enumerate(aa_in_window))))
+                index[1].extend(list(range(num_column - ws, num_column)))
+                current_row += 1
+                aa_in_window.pop(0)
+                aa_in_window.append(21 if j + ws > len(X[i]) else X[i][j + ws])
+                assert(len(aa_in_window) == 2 * ws + 1)
+        self.X = torch.sparse.FloatTensor(index, value, torch.Size([current_row, num_column]))
+        self.y = torch.FloatTensor(self.y)
+        self.cursor = 0
+        self.num_row = current_row
+        self.num_col = num_column
+        self.nonzero_col = 3 * ws + 1
+
+    def length(self):
+        return self.length
+
+    def has_next(self):
+        if self.cursor == self.num_row:
+            return False
+        return True
+
+    def reset(self):
+        self.cursor = 0
+
+    def next(self, batch_size=None):
+        if batch_size is None:
+            batch_size = min(self.num_row, 
+                    (1024 * 1024 * 1024) // (self.num_col * 8))
+        if batch_size == self.num_row:
+            self.cursor = self.num_row
+            return self.X.to_dense(), self.y
+        if self.cursor + batch_size >= self.num_row:
+            batch_size = self.num_row - self.cursor
+
+        start = self.cursor * self.nonzero_col
+        end = start + batch_size * self.nonzero_col
+        index = self.X._indices()
+        index = [index[0][start:end], index[1][start:end]]
+        value = self._values()
+        value = values[start:end]
+        self.cursor += batch_size
+        return torch.sparse.FloatTensor(index, value, 
+                torch.Size([batch_size, self.num_col])).to_dense(), 
+                y[self.cursor, self.cursor + batch_size]
+
+    def set_y_pred(self, y_pred):
+        current_row = 0
+        value = self.X._values()
+        for i in range(len(self.lengths)):
+            prev_y = [0.0] * self.ws
+            for j in range(len(self.lengths[i])):
+                start = current_row * self.num_col + 2 * self.ws + 1
+                end = start + self.ws
+                values[start:end] = prev_y
+                prev_y.pop(0)
+                prev_y.append(y_pred[current_row])
+                current_row += 1
+
+
+
 class ProteinDataset(torch.utils.data.Dataset):
 
     def __init__(self, X_files, y_files, cache_dir, limit=128*1024*1024, cache_prefix='cache-', validation=False):
