c1dfb22a9739a7fd31c56fe339aba7a20ec7879e
Ibraheem Moosa <ibraheemmoosa1347@gmail.com>
2018-09-21 19:38:32 +0600

    unnecessary code removed from main, dataset creation code added to main

--- src/torch_nn.py
+++ src/torch_nn.py
@@ -4,6 +4,11 @@ from torch.nn.functional import relu
 from torch.nn.functional import leaky_relu
 from torch.nn.functional import dropout
 import numpy as np
+import time
+import sys
+import os
+import random
+from Bio.PDB import Polypeptide
 
 class FeedForward(nn.Module):
     def __init__(self, input_size, num_layers):
@@ -47,11 +52,11 @@ class FeedForward(nn.Module):
     def train(self, num_iter, epoch_per_iter, dataset, validation_dataset):
         init_lr = 0.0005
         batch_size = 32
+        optimizer = torch.optim.Adam([{'params' : self.parameters(), 'initial_lr' : init_lr}],
+                            lr=init_lr, weight_decay=0.1)
+        scheduler = torch.optim.lr_scheduler.ExponentialLR(optimizer, 0.9)
         criterion = nn.MSELoss()
         for i in range(num_iter):
-            optimizer = optim.Adam([{'params' : self.parameters(), 'initial_lr' : init_lr}],
-                            lr=init_lr, weight_decay=0.1)
-            scheduler = optim.lr_scheduler.ExponentialLR(optimizer, 0.9)
             for epoch in range(epoch_per_iter):
                 scheduler.step()
                 while dataset.has_next():
@@ -91,7 +96,7 @@ class Dataset:
             for j in range(len(X[i])):
                 value.extend([1] * (2 * ws + 1))
                 value.extend([0] * ws)
-                index[0].extend([1] * (3 * ws + 1))
+                index[0].extend([current_row] * (3 * ws + 1))
                 index[1].extend(list(map(lambda t: 21 * t[0] + t[1], enumerate(aa_in_window))))
                 index[1].extend(list(range(num_column - ws, num_column)))
                 current_row += 1
@@ -159,159 +164,61 @@ def summarize_ndarrays(arrays):
     mins, maxs, means, stds = tuple(zip(*list(map(summarize_ndarray, arrays))))
     return np.mean(mins), np.mean(maxs), np.mean(means), np.mean(stds)
 
+def aa_to_index(aa):
+    """
+    :param aa: Three character amino acid name.
+    :returns: Integer index as per BioPython, unknown/non-standard amino acids return 20.
+    """
+    if Polypeptide.is_aa(aa, standard=True):
+        return Polypeptide.three_to_index(aa)
+    else:
+        return 20
+
+
 if __name__ == '__main__':
 
-    import sys
-
-    if len(sys.argv) < 9:
-        print('Usage: python3 regression.py data_dir X_files y_files validation_dir X_validation_files y_validation_files cache_dir checkpoint_dir [warm_start_model] [warm_start_epoch]')
-        print('Example: python3 regression.py X_9_18.npz y_9_18.npz 10')
+    if len(sys.argv) < 2:
+        print('Usage: python3 torch_nn.py data_dir')
         exit()
-
-    import time
     print(time.strftime('%Y-%m-%d %H:%M'))
-
-    import os
-    X_files_list = open(sys.argv[2])
-    X_files = []
-    for line in X_files_list:
-        X_files.append(os.path.join(sys.argv[1], line[:-1]))
-    y_files_list = open(sys.argv[3])
-    y_files = []
-    for line in y_files_list:
-        y_files.append(os.path.join(sys.argv[1], line[:-1]))
-    assert(len(X_files) == len(y_files))
-    '''
-    X_validation_files_list = open(sys.argv[5])
-    X_validation_files = []
-    for line in X_validation_files_list:
-        X_validation_files.append(os.path.join(sys.argv[4], line[:-1]))
-    y_validation_files_list = open(sys.argv[6])
-    y_validation_files = []
-    for line in y_validation_files_list:
-        y_validation_files.append(os.path.join(sys.argv[4], line[:-1]))
-    '''
-
-    import random
     random.seed(42)
-    indices = list(range(len(X_files)))
+    data_dir = sys.argv[1]
+    files = os.listdir(data_dir)
+    indices = list(range(len(files)))
     random.shuffle(indices)
 
-    X_files = [X_files[i] for i in indices]
-    y_files = [y_files[i] for i in indices]
-    X_validation_files = X_files[-len(indices) // 5:]
-    y_validation_files = y_files[-len(indices) // 5:]
-    X_files = X_files[:-len(indices) // 5]
-    y_files = y_files[:-len(indices) // 5]
-    indices = list(range(len(X_files)))
-    dataset = ProteinDataset(X_files, y_files, sys.argv[7], int(sys.argv[8]))
-    train_dataset = ProteinDataset(X_files, y_files, sys.argv[7], limit=int(sys.argv[8]), cache_prefix='train-', validation=True)
-    print('Dataset init done ', len(dataset))
-    validation_dataset = ProteinDataset(X_validation_files, y_validation_files, sys.argv[7], limit=int(sys.argv[8]), cache_prefix='validation-', validation=True)
-    print('Validation dataset init done ', len(validation_dataset))
+    train_files = [files[i] for i in indices[:int(len(indices * 0.80))]]
+    val_files = [files[i] for i in indices[int(len(indices * 0.80)):]]
+    train_Xes = []
+    train_ys = []
+    for fname in train_files:
+        with open(os.path.join(data_dir, fname)) as f:
+            train_Xes.append([])
+            train_ys.append([])
+            for line in f:
+                line = line.strip()
+                aa, b = line.split()
+                aa = aa_to_index(aa)
+                train_Xes[-1].append(aa)
+                train_ys[-1].append(b)
+
+    for fname in val_files:
+        with open(os.path.join(data_dir, fname)) as f:
+            val_Xes.append([])
+            val_ys.append([])
+            for line in f:
+                line = line.strip()
+                aa, b = line.split()
+                aa = aa_to_index(aa)
+                val_Xes[-1].append(aa)
+                val_ys[-1].append(b)
+
+
+    ws = 5
+    train_dataset = Dataset(train_Xes, train_ys, ws)
+    val_dataset = Dataset(val_Xes, val_ys, ws)
     print(time.strftime('%Y-%m-%d %H:%M'))
 
-    if len(sys.argv) == 11:
-        warm_start_model_params = torch.load(sys.argv[9])
-        warm_start_last_epoch = int(sys.argv[10])
-    else:
-        warm_start_model_params = None
-        warm_start_last_epoch = -1
+    num_layers = 8
+    net = FeedForward(dataset.num_col, num_layers)
 
-    batch_size = 256
-    dataloader = torch.utils.data.DataLoader(dataset, batch_size=batch_size, 
-            sampler=torch.utils.data.sampler.SequentialSampler(dataset))
-
-    print(dataset._num_of_features)
-    net = FeedForward(dataset._num_of_features)
-    if warm_start_model_params != None:
-        net.load_state_dict(warm_start_model_params)
-
-    from math import sqrt
-    import torch.optim as optim
-    init_lr = 0.0005
-    criterion = nn.MSELoss()
-    optimizer = optim.Adam([{'params' : net.parameters(), 'initial_lr' : init_lr}],
-                            lr=init_lr, weight_decay=0.1)
-    scheduler = optim.lr_scheduler.ExponentialLR(optimizer, 0.9, last_epoch=warm_start_last_epoch)
-    
-    from sklearn.metrics import r2_score
-    from scipy.stats import pearsonr
-    import math
-    best_validation_pcc = -math.inf
-    best_validation_pcc_found_at = -1
-
-    for epoch in range(warm_start_last_epoch + 1, warm_start_last_epoch + 1 + 200):
-        print('###############################################')
-        scheduler.step()
-        running_loss = 0.0
-        update_count = 0
-        for i, data in enumerate(dataloader):
-            update_count += 1
-            inputs, true_outputs = data
-            true_outputs = true_outputs.view(true_outputs.shape[0], 1)
-            optimizer.zero_grad()
-            predicted_outputs = net(inputs)
-            loss = criterion(predicted_outputs, 
-                                    true_outputs)
-            loss.backward()
-            optimizer.step()
-            running_loss += loss.item()
-        print('Epoch: {} done. Loss: {}'.format(
-                                epoch, running_loss / update_count))
-        print(time.strftime('%Y-%m-%d %H:%M'))
-        net.eval()
-        validation_pred, validation_true = net.predict(validation_dataset)
-        validation_pcc = list(map(lambda t: pearsonr(t[0], t[1])[0], zip(validation_pred, validation_true)))
-        validation_pcc = sum(validation_pcc) / len(validation_pcc)
-        validation_r2 = list(map(lambda t: r2_score(t[1], t[0]), zip(validation_pred, validation_true)))
-        validation_r2 = sum(validation_r2) / len(validation_r2)
-
-        if validation_pcc > best_validation_pcc:
-            best_validation_pcc = validation_pcc
-            best_validation_pcc_found_at = epoch
-        '''
-        train_pred, train_true = net.predict(train_dataset)
-        train_pcc = list(map(lambda t: pearsonr(t[0], t[1])[0], zip(train_pred, train_true)))
-        train_pcc = sum(train_pcc) / len(train_pcc)
-        train_r2 = list(map(lambda t: r2_score(t[1], t[0]), zip(train_pred, train_true)))
-        train_r2 = sum(train_r2) / len(train_r2)
-        '''
-        #R2 = 1 - (mean squared error) / (Variance of truth)
-        print('-----------------------------------------------')
-        print('Validation Prediction min, max, mean, std: {}'.format(summarize_ndarrays(validation_pred)))
-        print('Validation True min, max, mean, std: {}'.format(summarize_ndarrays(validation_true)))
-        print('Validation PCC: {}'.format(validation_pcc))
-        print('Validation R2: {}'.format(validation_r2))
-        print('-----------------------------------------------')
-        '''
-        print('Train Prediction min, max, mean, std: {}'.format(summarize_ndarrays(train_pred)))
-        print('Train True min, max, mean, std: {}'.format(summarize_ndarrays(train_true)))
-        print('Train PCC: {}'.format(train_pcc))
-        print('Train R2: {}'.format(train_r2))
-        print('-----------------------------------------------')
-        '''
-        for name, param in net.named_parameters():
-            print(name)
-            print(summarize_tensor(param))
-        print('###############################################')
-        print(time.strftime('%Y-%m-%d %H:%M'))
-    #    for g in optimizer.param_groups:
-    #        g['lr'] = init_lr / sqrt(epoch + 1)
-        #if (epoch + 1) % 5 == 0:
-        torch.save(net.state_dict(), os.path.join(sys.argv[8], 'net-{0:02d}'.format(epoch)))
-        if epoch - best_validation_pcc_found_at >= 10:
-            break
-        net.train()
-        random.shuffle(indices)
-        X_files = [X_files[i] for i in indices]
-        y_files = [y_files[i] for i in indices]
-        dataset = ProteinDataset(X_files, y_files, sys.argv[7], int(sys.argv[8]))
-        dataloader = torch.utils.data.DataLoader(dataset, batch_size=batch_size, 
-                     sampler=torch.utils.data.sampler.SequentialSampler(dataset))
-    '''
-    save_net = input('Do you want to save the net?')
-    if 'y' in save_net:
-        fname = input('File Name;')
-        torch.save(net.state_dict(), fname)
-    '''
